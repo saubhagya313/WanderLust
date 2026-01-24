d9fbef6fade10be4881749269a56b6c2b9752291
saubhagya313 <saubhagyasethi6@gmail.com>
2026-01-24 23:47:56 +0530

    image addition

--- routes/listing.js
+++ routes/listing.js
@@ -7,15 +7,12 @@ const wrapasync=require("../utils/wrapasync");  //wrap asynic file require
 const expresserror=require("../utils/expresserror.js");   //express error  class require
 const {ListingSchema,reviewSchema}=require("../schema.js");
 const {islogged,canChange,validateschema}=require("../middleware.js");
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 const listingController=require("../controller/listing.js");
 
 
-
-router.route("/:id")  //here we deined path once so dont need to define it for all 
-.put(islogged,canChange,validateschema,wrapasync(listingController.updateEdited))
-.get(wrapasync(listingController.showDetails))
 
 
 //rest are havig different paths
@@ -24,10 +21,18 @@ router.get("/", wrapasync(listingController.index))
 
 router.get("/new", islogged,listingController.newformRender)
 
+router.route("/:id")  //here we deined path once so dont need to define it for all 
+.put(islogged,canChange,validateschema,wrapasync(listingController.updateEdited))
+.get(wrapasync(listingController.showDetails))
+
+
 router.get("/:id/edit", islogged,canChange,wrapasync(listingController.editListing))
 
 
-router.post("/add", validateschema,wrapasync(listingController.createNewListing))
+router.post("/add",upload.single('Listing[image]'),(req,res)=>{
+   
+    res.send(req.file);
+})
 
 
 router.delete("/:id/del", canChange,wrapasync(listingController.deleteListing))
